0befe2c3ccbd968c63aac987d1c6a9b291c84cda
Pratik Wani <pwani8306@gmail.com>
2026-08-27 14:50:20 +0530

    feat: add protect route

---
+++ client/src/components/ProtectedRoute.jsx
@@ -0,0 +1,15 @@
+import { Navigate } from "react-router-dom";
+
+export default function ProtectedRoute({
+  children,
+}) {
+  const token =
+    localStorage.getItem("token") ||
+    sessionStorage.getItem("token");
+
+  if (!token) {
+    return <Navigate to="/login-role-portal" replace />;
+  }
+
+  return children;
+}
